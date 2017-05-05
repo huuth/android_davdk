9391b8d2cfdccdfcb7b5fde0d470452b82e81238
Thuy Tien <thuytien@Macintoshs-MacBook-Air.local>
2017-05-05 10:59:16 +0700

    branch donne

--- app/src/main/java/com/example/hawking/geturltest/baoduongFragment.java
+++ app/src/main/java/com/example/hawking/geturltest/baoduongFragment.java
@@ -2,11 +2,18 @@ package com.example.hawking.geturltest;
 
 
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
@@ -21,9 +28,56 @@ import java.net.URL;
  */
 
 public class baoduongFragment extends Fragment {
+
+    String urlBaoDuong = "http://davdk.tk/index.php?ctr=statusmotor&act=get_status_mobile&id_device=1";
+    ProgressBar progressDau, progressBarLoc, progressBarBugi, progressBarXich;
+    TextView txtPercentageBugi, txtPercentageXich, txtPercentageDau, txtPercentageLoc;
+    EditText txtTong;
     @Nullable
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_baoduong,container,false);
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
+        progressDau = (ProgressBar) view.findViewById(R.id.progressDau);
+        progressBarBugi = (ProgressBar) view.findViewById(R.id.progressBarBugi);
+        progressBarLoc = (ProgressBar) view.findViewById(R.id.progressBarLoc);
+        progressBarXich = (ProgressBar) view.findViewById(R.id.progressBarXich);
+
+        txtPercentageBugi = (TextView) view.findViewById(R.id.txtPercentageBugi);
+        txtPercentageDau = (TextView) view.findViewById(R.id.txtPercentageDau);
+        txtPercentageLoc = (TextView) view.findViewById(R.id.txtPercentageLoc);
+        txtPercentageXich = (TextView) view.findViewById(R.id.txtPercentageXich);
+
+        txtTong = (EditText) view.findViewById(R.id.txtTong);
+
+        try {
+            String jsonStr = readJsonFromUrl(urlBaoDuong);
+            JSONObject json = new JSONObject(jsonStr);
+            System.out.println("jsoooooonnnn: " + json.toString());
+            progressBarBugi.setMax(100);
+            progressBarBugi.setProgress((int) Math.ceil(Float.parseFloat(json.getString("bugi"))));
+            txtPercentageBugi.setText((int) Math.ceil(Float.parseFloat(json.getString("bugi"))) + "%");
+
+            progressBarXich.setMax(100);
+            progressBarXich.setProgress((int) Math.ceil(Float.parseFloat(json.getString("chain"))));
+            txtPercentageXich.setText((int) Math.ceil(Float.parseFloat(json.getString("chain"))) + "%");
+
+            progressBarLoc.setMax(100);
+            progressBarLoc.setProgress((int) Math.ceil(Float.parseFloat(json.getString("air_filter"))));
+            txtPercentageLoc.setText((int) Math.ceil(Float.parseFloat(json.getString("air_filter"))) + "%");
+
+            progressDau.setMax(100);
+            progressDau.setProgress((int) Math.ceil(Float.parseFloat(json.getString("lubricant"))));
+            txtPercentageDau.setText((int) Math.ceil(Float.parseFloat(json.getString("lubricant"))) + "%");
+
+            txtTong.setText(json.getString("total_distance")+" km");
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
         return view;
     }
 
